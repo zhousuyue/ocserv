db23cca692cbea2a08d8d1407b81d9c3b64e7717
zhousuyue <67529189+zhousuyue@users.noreply.github.com>
2024-01-31 02:02:31 +0800

    Update ocserv_radius_quickinstall.sh

--- ocserv_radius_quickinstall.sh
+++ ocserv_radius_quickinstall.sh
@@ -1,6 +1,6 @@
 sudo -i
 yum install wget -y 
-wget https://raw.githubusercontent.com/chendong12/ocserv/master/radius_for_ocserv.sh
+wget https://raw.githubusercontent.com/zhousuyue/ocserv/master/radius_for_ocserv.sh
 chmod +x radius_for_ocserv.sh
 ./radius_for_ocserv.sh
 #
